1e9ee0c52f5c5726ab314d577abd86c7321ae600
SebNik <lord.voldemort1492@gmail.com>
2020-02-01 20:49:09 +0100

    Added API-Key. Collecting data of real stock example usage and plot

--- GetStockDate.py
+++ GetStockDate.py
@@ -1,7 +1,10 @@
 from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
-ts = TimeSeries(key='YOUR_API_KEY',output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='TSE:TD',interval='1min', outputsize='full')
+
+API_KEY = 'U5C8JI4ELG45JNT7'
+
+ts = TimeSeries(key=API_KEY,output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
 print(data)
 data['4. close'].plot()
 plt.title('Intraday TimeSeries Google')
