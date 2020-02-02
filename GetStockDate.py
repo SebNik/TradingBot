b0672e7331c809c4dc39df677b37667c6f6467cb
SebNik <lord.voldemort1492@gmail.com>
2020-02-02 10:42:38 +0100

    Working function for intraday StockData

--- GetStockDate.py
+++ GetStockDate.py
@@ -1,14 +1,17 @@
-from alpha_vantage.timeseries import TimeSeries
-import csv
-import pandas as pd
+def get_data_intraday(symbol,interval,outputsize,API_KEY,savingtoCsv=True):
+    from alpha_vantage.timeseries import TimeSeries
+    import csv
 
-API_KEY = 'U5C8JI4ELG45JNT7'
-symbol='MSFT'
-interval='1min'
-outputsize='compact'
+    ts = TimeSeries(key=API_KEY,output_format='pandas')
+    data, meta_data = ts.get_intraday(symbol=symbol,interval=interval, outputsize=outputsize)
+    if savingtoCsv:
+        data.to_csv('/home/niklas/Desktop/TradingBot/StockData/' + 'StockData-' + symbol + '-' + interval +'.csv')
+    return data,meta_data
 
-ts = TimeSeries(key=API_KEY,output_format='pandas')
-data, meta_data = ts.get_intraday(symbol=symbol,interval=interval, outputsize=outputsize)
-
-print(data.head())
-data.to_csv('/home/niklas/Desktop/TradingBot/StockData/' + 'StockData-' + symbol + '-' + interval +'.csv')
+if __name__ == "__main__":
+    API_KEY = 'U5C8JI4ELG45JNT7'
+    symbol='MSFT'
+    interval='1min'
+    outputsize='compact'
+    data,meta_data=get_data_intraday(symbol,interval,outputsize,API_KEY)
+    print(data.head())
