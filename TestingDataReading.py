c15fac879b359b8bcfc9ca71490d3d6f1e62f7d3
SebNik <lord.voldemort1492@gmail.com>
2020-02-02 13:41:31 +0100

    Added log files for the tests

--- TestingDataReading.py
+++ TestingDataReading.py
@@ -14,7 +14,9 @@ while True:
     time=strftime("%Y-%m-%d %A %H:%M:%S", gmtime())
     time_day=strftime("%A", gmtime())
     time_hour=strftime("%H", gmtime())
-    #print(time)
+    f = open("log.txt", "w")
+    f.write(time)
+    f.close()
     if time_day!="Sunday" and time_day!="Saturday" and int(time_hour)-4>8 and int(time_hour)-4<17:
         d,m=GetStockDate.get_data_intraday(API_KEY,symbol,interval,outputsize)
         print(GetStockDate.get_data_latest(API_KEY,symbol)['05. price']+'------------'+d.head(1))
