9ab7bf1d75e164c5ad0d4468c04ec6424ed531bc
SebNik <lord.voldemort1492@gmail.com>
2020-02-03 14:38:44 +0100

    Fixed test logs but still bullshit outprint

--- TestingDataReading.py
+++ TestingDataReading.py
@@ -3,6 +3,7 @@
 
 import GetStockDate
 from time import gmtime, strftime,sleep
+import pandas as pd
 
 symbol='AAPL'
 interval='1min'
@@ -13,11 +14,12 @@ while True:
     time_day=strftime("%A", gmtime())
     time_hour=strftime("%H", gmtime())
     f = open("log.txt", "a")
-    if time_day!="Sunday" and time_day!="Saturday" and int(time_hour)-4>8 and int(time_hour)-4<17:
+    if time_day!="Sunday" and time_day!="Saturday" and int(time_hour)-5>7 and int(time_hour)-5<17:
         d,m=GetStockDate.get_data_intraday(symbol,interval,outputsize)
-        print(GetStockDate.get_data_latest(symbol)['05. price']+'------------'+d.head(1))
-        f.write(GetStockDate.get_data_latest(symbol)['05. price']+'------------'+d.head(1))
+        latest_data=GetStockDate.get_data_latest(symbol)['05. price']
+        #print(latest_data+'------------'+d.head(1))
+        f.write(latest_data.to_string(index=False)+'------------'+d.head(1)['4. close'].to_string(index=False)+'\n'+'-------------'+'\n')
     else:
-        f.write(time+'\n'+time_day+time_hour)
+        f.write(time+time_day+time_hour+'\n')
     f.close()
     sleep((7*60*60)/500)
