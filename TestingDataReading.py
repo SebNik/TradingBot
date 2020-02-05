414c2b0a0f8edf53ffa13cda546cb89883c00fff
SebNik <lord.voldemort1492@gmail.com>
2020-02-05 16:08:57 +0100

    Fixed Bugs in Testingfile

--- TestingDataReading.py
+++ TestingDataReading.py
@@ -15,7 +15,8 @@ while True:
     f = open("log.txt", "a")
     if time_day!="Sunday" and time_day!="Saturday" and int(time_hour)-5>9 and int(time_hour)-5<17:
         d,m,r=GetStockDate.get_data_intraday(symbol,interval,outputsize)
-        latest_data=GetStockDate.get_data_latest(symbol)['05. price']
+        latest_data,r=GetStockDate.get_data_latest(symbol)
+        latest_data=latest_data['05. price']
         #print(latest_data+'------------'+d.head(1))
         f.write(latest_data.to_string(index=False)+'------------'+d.head(1)['4. close'].to_string(index=False)+'\n'+'-------------'+'\n')
     else:
