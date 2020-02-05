d8d33a67fab8199e608fe1195af2848a95df24b3
SebNik <lord.voldemort1492@gmail.com>
2020-02-05 16:04:13 +0100

    Added api calling and resoulution data and tests

--- GetStockDate.py
+++ GetStockDate.py
@@ -46,44 +46,35 @@ def api_key_finder():
         f.write(time+'-'+str(no)+'-'+selected_api_key+'-'+'0'+'\n')
     f.close()
     #closing and saving file
-    return selected_api_key
+    return selected_api_key,str(resolution_data)
 
 
 def get_data_intraday(symbol,interval,outputsize,savingtoCsv=True):
     #gets data over a periode of a day
     from alpha_vantage.timeseries import TimeSeries
-    API_KEY = 'U5C8JI4ELG45JNT7'
+    API_KEY,waiting_times=api_key_finder()
     ts = TimeSeries(key=API_KEY,output_format='pandas')
     data, meta_data = ts.get_intraday(symbol=symbol,interval=interval, outputsize=outputsize)
     if savingtoCsv:
         data.to_csv('/home/niklas/Desktop/TradingBot/StockData/' + 'StockData-' + symbol + '-' + interval +'.csv',sep=';')
-    return data,meta_data
+    return data,meta_data,waiting_times
 
 def get_data_latest(symbol,savingtoCsv=False):
     #reads the latest data of the API
     #get changes of the last day
     from alpha_vantage.timeseries import TimeSeries
-
+    API_KEY,waiting_times=api_key_finder()
     ts = TimeSeries(key=API_KEY,output_format='pandas')
     data = ts.get_quote_endpoint(symbol=symbol)
-    return data[0]
+    return data[0],waiting_times
 
 if __name__ == "__main__":
-    API_KEY = 'U5C8JI4ELG45JNT7'
-    API_KEY_2='L7C6HSQARL8LR5E4'
-    API_KEY_3='D7TUJ5FRXFV44XPO'
-    API_KEY_4='2EGXAE0H594DZ9U5'
     symbol='MSFT'
     interval='1min'
     outputsize='compact'
-    #data,meta_data=get_data_intraday(API_KEY,symbol,interval,outputsize)
-    #print(meta_data)
-    #print(data.head(1))
-    #print(get_data_latest(API_KEY,symbol)['05. price'])
-    print(api_key_finder())
-    print(api_key_finder())
-    print(api_key_finder())
-    print(api_key_finder())
-    print(api_key_finder())
-    print(api_key_finder())
-    print(api_key_finder())
+    data,meta_data,waiting_times=get_data_intraday(symbol,interval,outputsize)
+    data_latest,waiting_times=get_data_latest(symbol)
+    print(waiting_times)
+    print(meta_data)
+    print(data.head(1))
+    print(data_latest['05. price'])
