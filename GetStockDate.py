cc1a38798816340d7d77af57629efce645c95847
SebNik <lord.voldemort1492@gmail.com>
2020-02-05 06:39:10 +0100

    Started developing apikeyfinder

--- GetStockDate.py
+++ GetStockDate.py
@@ -2,13 +2,47 @@
 
 def api_key_finder():
     #selctes the right api key for maximal sucees
+    from time import gmtime, strftime
+    import os
     #500 requests a day -- 5 per min
+    file_api='api-key_logs.txt'
     api_keys=['U5C8JI4ELG45JNT7','L7C6HSQARL8LR5E4','D7TUJ5FRXFV44XPO','2EGXAE0H594DZ9U5']
-    trading_hours=7
+    selected_api_key=''
+    #log format:%Y+%m+%d-%A+%H:%M:%S-APIKEYNO-APIKEY-requests
+    trading_hours=6.5
     resolution_data=((trading_hours*60*60)/500)/len(api_keys)
     #gives how many seconds are between possible requests
-    if resolution_data<6:
-        #not enough keys to many requests 5 requests per min
+    calls_per_key_per_min=60/resolution_data*len(api_keys)
+    if calls_per_key_per_min<6:
+        #not possible
+        while calls_per_key_per_min<6:
+            calls_per_key_per_min=60/resolution_data*len(api_keys)
+            resolution_data+=0.25
+    #ideal call time is resoulution_data
+    if (os.path.isfile(file_api)==False):
+        f=open(file_api, 'x')
+        f.write('-Logs of APIs-\n')
+        f.close()
+    f=open(file_api, 'r')
+    api_key_logs=f.readlines()
+    last_api_key_log=api_key_logs[-1].split('-')
+    f.close()
+    time=strftime("%Y+%m+%d-%A+%H:%M:%S", gmtime())
+    time_day=strftime("%Y+%m+%d", gmtime())
+    #opens for logging
+    f=open(file_api, 'a')
+    #starting to check
+    if (last_api_key_log[0]==time_day):
+        if (api_keys[int(last_api_key_log[2])]==last_api_key_log[3]):
+            selected_api_key=api_keys[int(last_api_key_log[2])+1]
+            f.write(time+'-'+str(int(last_api_key_log[2])+1)+'-'+selected_api_key+'-'+str(int(last_api_key_log[4])+1)+'\n')
+    else:
+        from random import randint
+        no=randint(0,len(api_keys)-1)
+        selected_api_key=api_keys[no]
+        f.write(time+'-'+str(no)+'-'+selected_api_key+'-'+'0'+'\n')
+    f.close()
+    #closing and saving file
 
 
 def get_data_intraday(symbol,interval,outputsize,savingtoCsv=True):
@@ -25,7 +59,7 @@ def get_data_latest(symbol,savingtoCsv=False):
     #reads the latest data of the API
     #get changes of the last day
     from alpha_vantage.timeseries import TimeSeries
-    API_KEY=
+
     ts = TimeSeries(key=API_KEY,output_format='pandas')
     data = ts.get_quote_endpoint(symbol=symbol)
     return data[0]
@@ -38,7 +72,15 @@ if __name__ == "__main__":
     symbol='MSFT'
     interval='1min'
     outputsize='compact'
-    data,meta_data=get_data_intraday(API_KEY,symbol,interval,outputsize)
-    print(meta_data)
-    print(data.head(1))
-    print(get_data_latest(API_KEY,symbol)['05. price'])
+    #data,meta_data=get_data_intraday(API_KEY,symbol,interval,outputsize)
+    #print(meta_data)
+    #print(data.head(1))
+    #print(get_data_latest(API_KEY,symbol)['05. price'])
+    print(api_key_finder())
+    print(api_key_finder())
+    print(api_key_finder())
+    print(api_key_finder())
+    print(api_key_finder())
+    print(api_key_finder())
+    print(api_key_finder())
+    print(api_key_finder())
