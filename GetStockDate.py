251d8be016f334da716ddc21adbb572b6128ede5
Niklas <lord.voldemort1492@gmail.com>
2020-03-22 17:37:22 +0100

    Started working in PyCharm, reorganised all file so far

--- GetStockDate.py
+++ GetStockDate.py
@@ -1,88 +1,95 @@
-#all data is from GMT-5 -> Time Germany -6
-
+# all data is from GMT-5 -> Time Germany -6
 def api_key_finder():
-    #selctes the right api key for maximal sucees
+    # selects the right api key for maximal success
     from time import gmtime, strftime
     import os
-    #500 requests a day -- 5 per min
-    file_api='api-key_logs.txt'
-    api_keys=['U5C8JI4ELG45JNT7','L7C6HSQARL8LR5E4','D7TUJ5FRXFV44XPO','2EGXAE0H594DZ9U5','UNK3NBPC8S27EKHN','ZX686301JW1AMF8I','DI2GYUJXWL8OL030']
-    selected_api_key=''
-    #log format:%Y+%m+%d-%A+%H:%M:%S-APIKEYNO-APIKEY-requests
-    closing_sec=16*60*60
-    trading_sec=closing_sec-(((int(strftime("%H", gmtime()))-5)*60)+int(strftime("%M", gmtime())))*60
-    #how many seconds are left for the day in trading
-    resolution_data=(trading_sec/500)/len(api_keys)
-    #print(resolution_data)
-    #gives how many seconds are between possible requests
-    calls_per_key_per_min=60/resolution_data*len(api_keys)
-    if calls_per_key_per_min<6:
-        #not possible
-        while calls_per_key_per_min<6:
-            calls_per_key_per_min=60/resolution_data*len(api_keys)
-            resolution_data+=0.25
-    #ideal call time is resoulution_data
-    if (os.path.isfile(file_api)==False):
-        f=open(file_api, 'x')
+    # 500 requests a day -- 5 per min
+    file_api = 'api-key_logs.txt'
+    api_keys = ['U5C8JI4ELG45JNT7', 'L7C6HSQARL8LR5E4', 'D7TUJ5FRXFV44XPO', '2EGXAE0H594DZ9U5', 'UNK3NBPC8S27EKHN',
+                'ZX686301JW1AMF8I', 'DI2GYUJXWL8OL030']
+    selected_api_key = ''
+    # log format:%Y+%m+%d-%A+%H:%M:%S-APIKEYNO-APIKEY-requests
+    closing_sec = 16 * 60 * 60
+    trading_sec = closing_sec - (((int(strftime("%H", gmtime())) - 5) * 60) + int(strftime("%M", gmtime()))) * 60
+    # how many seconds are left for the day in trading
+    resolution_data = (trading_sec / 500) / len(api_keys)
+    # print(resolution_data)
+    # gives how many seconds are between possible requests
+    calls_per_key_per_min = 60 / resolution_data * len(api_keys)
+    if calls_per_key_per_min < 6:
+        # not possible
+        while calls_per_key_per_min < 6:
+            calls_per_key_per_min = 60 / resolution_data * len(api_keys)
+            resolution_data += 0.25
+    # ideal call time is resoulution_data
+    if not os.path.isfile(file_api):
+        f = open(file_api, 'x')
         f.write('-Logs of APIs-\n')
         f.close()
-    f=open(file_api, 'r')
-    api_key_logs=f.readlines()
-    last_api_key_log=api_key_logs[-1].split('-')
+    f = open(file_api, 'r')
+    api_key_logs = f.readlines()
+    last_api_key_log = api_key_logs[-1].split('-')
     f.close()
-    time=strftime("%Y+%m+%d-%A+%H:%M:%S", gmtime())
-    time_day=strftime("%Y+%m+%d", gmtime())
-    #opens for logging
-    f=open(file_api, 'a')
-    #starting to check
-    if (last_api_key_log[0]==time_day):
-        if (api_keys[int(last_api_key_log[2])]==last_api_key_log[3]):
-            no=int(last_api_key_log[2])+1
-            if (no==len(api_keys)):
-                no=0
-            selected_api_key=api_keys[no]
-            f.write(time+'-'+str(no)+'-'+selected_api_key+'-'+str(int(last_api_key_log[4])+1)+'\n')
+    time = strftime("%Y+%m+%d-%A+%H:%M:%S", gmtime())
+    time_day = strftime("%Y+%m+%d", gmtime())
+    # opens for logging
+    f = open(file_api, 'a')
+    # starting to check
+    if last_api_key_log[0] == time_day:
+        if api_keys[int(last_api_key_log[2])] == last_api_key_log[3]:
+            no = int(last_api_key_log[2]) + 1
+            if no == len(api_keys):
+                no = 0
+            selected_api_key = api_keys[no]
+            f.write(time + '-' + str(no) + '-' + selected_api_key + '-' + str(int(last_api_key_log[4]) + 1) + '\n')
     else:
         from random import randint
-        no=randint(0,len(api_keys)-1)
-        selected_api_key=api_keys[no]
-        f.write(time+'-'+str(no)+'-'+selected_api_key+'-'+'0'+'\n')
+        no = randint(0, len(api_keys) - 1)
+        selected_api_key = api_keys[no]
+        f.write(time + '-' + str(no) + '-' + selected_api_key + '-' + '0' + '\n')
     f.close()
-    #closing and saving file
-    return selected_api_key,str(resolution_data)
+    # closing and saving file
+    return selected_api_key, str(resolution_data)
 
 
-def get_data_intraday(symbol,interval,outputsize,savingtoCsv=True):
-    #gets data over a periode of a day
+def get_data_intraday(symbol, interval, outputsize, savingtoCsv=True):
+    # gets data over a periode of a day
     from alpha_vantage.timeseries import TimeSeries
     from time import gmtime, strftime
-    #time for saving
-    API_KEY,waiting_times=api_key_finder()
-    ts = TimeSeries(key=API_KEY,output_format='pandas')
-    time=strftime("%Y-%m-%d-%A", gmtime())
-    data, meta_data = ts.get_intraday(symbol=symbol,interval=interval, outputsize=outputsize)
+    # time for saving
+    API_KEY, waiting_times = api_key_finder()
+    ts = TimeSeries(key=API_KEY, output_format='pandas')
+    time = strftime("%Y-%m-%d-%A", gmtime())
+    data, meta_data = ts.get_intraday(symbol=symbol, interval=interval, outputsize=outputsize)
     if savingtoCsv:
-        data.to_csv('/home/niklas/Desktop/TradingBot/StockData/' + 'StockData-' + symbol + '-' + interval + '-' + time + '.csv',sep=';')
-        #saved data csv-file data
-    return data,meta_data,waiting_times
+        data.to_csv(
+            '/home/niklas/Desktop/TradingBot/StockData/' + 'StockData-' + symbol + '-' + interval + '-' + time + '.csv',
+            sep=';')
+        # saved data csv-file data
+    return data, meta_data, waiting_times
 
-def get_data_latest(symbol,savingtoCsv=False):
-    #reads the latest data of the API
-    #get changes of the last day
+
+def get_data_latest(symbol, savingtoCsv=False):
+    # reads the latest data of the API
+    # get changes of the last day
     from alpha_vantage.timeseries import TimeSeries
-    API_KEY,waiting_times=api_key_finder()
-    ts = TimeSeries(key=API_KEY,output_format='pandas')
+    API_KEY, waiting_times = api_key_finder()
+    ts = TimeSeries(key=API_KEY, output_format='pandas')
     data = ts.get_quote_endpoint(symbol=symbol)
-    return data[0],waiting_times
+    return data[0], waiting_times
+
 
 if __name__ == "__main__":
-    symbol='MSFT'
-    interval='1min'
-    outputsize='compact'
-    for i in range(0,10):
-        data,meta_data,waiting_times=get_data_intraday(symbol,interval,outputsize)
-        data_latest,waiting_times=get_data_latest(symbol)
+    import time
+
+    symbol = 'MSFT'
+    interval = '1min'
+    outputsize = 'compact'
+    for i in range(0, 10):
+        data, meta_data, waiting_times = get_data_intraday(symbol, interval, outputsize)
+        data_latest, waiting_times = get_data_latest(symbol)
         print(waiting_times)
         print(meta_data)
         print(data.head(1))
         print(data_latest['05. price'])
+        time.sleep(5)
--- TestingDataReading.py
+++ TestingDataReading.py
@@ -1,26 +1,27 @@
-#This program will test if the realtime data works
-#Moreover it will test if the latest data works as well
+# This program will test if the realtime data works
+# Moreover it will test if the latest data works as well
 
 import GetStockDate
-from time import gmtime, strftime,sleep
+from time import gmtime, strftime, sleep
 
-symbol='AAPL'
-interval='1min'
-outputsize='compact'
+symbol = 'AAPL'
+interval = '1min'
+outputsize = 'compact'
 
 while True:
-    time=strftime("%Y-%m-%d %A %H:%M:%S", gmtime())
-    time_day=strftime("%A", gmtime())
-    time_hour=strftime("%H", gmtime())
+    time = strftime("%Y-%m-%d %A %H:%M:%S", gmtime())
+    time_day = strftime("%A", gmtime())
+    time_hour = strftime("%H", gmtime())
     f = open("log.txt", "a")
-    r=60
-    if time_day!="Sunday" and time_day!="Saturday" and int(time_hour)-5>9 and int(time_hour)-5<17:
-        d,m,r=GetStockDate.get_data_intraday(symbol,interval,outputsize)
-        latest_data,r=GetStockDate.get_data_latest(symbol)
-        latest_data=latest_data['05. price']
-        #print(latest_data+'------------'+d.head(1))
-        f.write(latest_data.to_string(index=False)+'------------'+d.head(1)['4. close'].to_string(index=False)+'\n'+'-------------'+'\n')
+    r = 60
+    if time_day != "Sunday" and time_day != "Saturday" and int(time_hour) - 5 > 9 and int(time_hour) - 5 < 17:
+        d, m, r = GetStockDate.get_data_intraday(symbol, interval, outputsize)
+        latest_data, r = GetStockDate.get_data_latest(symbol)
+        latest_data = latest_data['05. price']
+        # print(latest_data+'------------'+d.head(1))
+        f.write(latest_data.to_string(index=False) + '------------' + d.head(1)['4. close'].to_string(
+            index=False) + '\n' + '-------------' + '\n')
     else:
-        f.write(time+time_day+time_hour+'\n')
+        f.write(time + time_day + time_hour + '\n')
     f.close()
     sleep(float(r))
