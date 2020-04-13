89eaa6a4eec8d152d3ad6aeaf85ec17ab20bf4dc
Niklas <lord.voldemort1492@gmail.com>
2020-04-13 19:38:19 +0200

    Added Trade class

--- TradeStocks.py
+++ TradeStocks.py
@@ -1,4 +1,9 @@
 # this file is responsible for all buying and selling actions and their logging
+# the class Trade will be responsible for all actions
+
+class Trade:
+    def __init__(self):
+        None
 
 
 if __name__ == "__main__":
