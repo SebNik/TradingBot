d03bf75cf1d42e5a93516138cf7cf89ac748f612
Niklas <lord.voldemort1492@gmail.com>
2020-04-15 20:17:51 +0200

    Able to merge

--- TradeStocks.py
+++ TradeStocks.py
@@ -1,10 +1,35 @@
 # this file is responsible for all buying and selling actions and their logging
-# the class Trade will be responsible for all actions
-
-class Trade:
+class Stock:
     def __init__(self):
         None
 
 
+def check_database():
+    import sqlite3
+    conn = sqlite3.connect('/home/niklas/Desktop/TradingBot/StockData/StockData-TSLA.db')
+    c = conn.cursor()
+    # tableName = x + '2'
+    # stmt = "SELECT name FROM sqlite_master WHERE type='table' AND name='{}';".format(tableName)
+    # c.execute(stmt)
+    # result = c.fetchone()
+    # if result:
+    #     # there is a table named "tableName"
+    #     print('existing')
+    # else:
+    #     # there are no tables named "tableName"
+    #     print('not found')
+    #
+    # # Insert a row of data
+    # sql = "INSERT INTO {} VALUES".format(x + '2')
+    # c.execute(sql + " ('2006-01-05','BUY','RHAT', {} ,35.14)".format(1562))
+    # # c.execute("INSERT INTO ? VALUES (?,?,?,5,5);", ('213', 'RHAT', '5'),(x))
+    # conn.commit()
+    data = c.execute("SELECT * FROM {}".format('IntraDayTSLA1min'))
+    print(data)
+    for i in data:
+        print(i)
+    conn.close()
+
+
 if __name__ == "__main__":
-    None
+    check_database()
