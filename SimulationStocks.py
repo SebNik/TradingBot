bf61ebe40b691824ed7a37caccabc400cdaf0c5c
Niklas <lord.voldemort1492@gmail.com>
2020-04-16 18:21:59 +0200

    Stared working on simulation stock prices

---
+++ SimulationStocks.py
@@ -0,0 +1,12 @@
+# this file is responsible to manipulate stock prices in any given way
+# the easiest option would be to to this with historical dat
+
+
+class Simulation:
+    def __init__(self,interval='daily'):
+        if interval=='daily':
+            None
+        if interval=='monthly':
+            None
+        if interval=='intrday':
+            None
