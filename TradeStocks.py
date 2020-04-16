cf4c87b3337a27c0d5643109f472f78df59e9561
Niklas <lord.voldemort1492@gmail.com>
2020-04-16 09:50:46 +0200

    added a few basic function for sturcture

--- TradeStocks.py
+++ TradeStocks.py
@@ -8,6 +8,28 @@ class Stock:
         self.account=start_acc
         self.table_name=symbol.upper()+'-Account'
         self.broker_fee=fee
+        self.units=0
+
+
+    def log_to_database(self):
+        None
+
+
+    def read_stock_price(self):
+        None
+
+
+    def change(self, value):
+        self.account+=value
+        self.log_to_database()
+
+
+    def buy(self,units_to_buy):
+        None
+
+
+    def sell(self, units_to_sell):
+        None
 
 
 if __name__ == "__main__":
