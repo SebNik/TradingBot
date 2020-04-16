b49d95b550f98b67f5e67b59d48a6becbceab295
Niklas <lord.voldemort1492@gmail.com>
2020-04-16 09:41:45 +0200

    added init function

--- TradeStocks.py
+++ TradeStocks.py
@@ -3,8 +3,11 @@
 
 
 class Stock:
-    def __init__(self):
-        None
+    def __init__(self, symbol, start_acc=1000, fee=0.01):
+        self.symbol=symbol
+        self.account=start_acc
+        self.table_name=symbol.upper()+'-Account'
+        self.broker_fee=fee
 
 
 if __name__ == "__main__":
