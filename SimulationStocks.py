d991d2b66d9eb2ed30be11399627fa371377d424
Niklas <lord.voldemort1492@gmail.com>
2020-04-17 18:48:43 +0200

    Trying to build range funcion

--- SimulationStocks.py
+++ SimulationStocks.py
@@ -3,9 +3,12 @@
 
 
 class Simulation:
-    def __init__(self, symbol, interval='daily'):
+    def __init__(self, symbol, interval='daily', date_range=None):
         # loading in modules
         import GetStockData
+        import sqlite3
+        import pandas as pd
+        import datetime
         # setting self.vars
         self.symbol = symbol
         self.interval = interval
@@ -23,6 +26,26 @@ class Simulation:
         if self.interval == 'intraday':
             GetStockData.get_data_intraday(symbol, '1min', 'full', savingtoCsv=False)
             self.table_name = 'IntraDay' + self.symbol + 'min'
+        # getting pandas document
+        # connecting to database
+        conn = sqlite3.connect(self.file)
+        # reading the database into  pandas dataframe
+        df = pd.read_sql_query("SELECT * FROM {}".format(self.table_name), conn)
+        # filtering data
+        if date_range is not None:
+            # creating list with dates
+            self.date1 = datetime.datetime(date_range[0][0],date_range[0][1],date_range[0][2])
+            self.date2 = datetime.datetime(date_range[1][0],date_range[1][1],date_range[1][2])
+            self.date_range = date_range
+            dates_list = pd.bdate_range(self.date1, self.date2)
+            print(dates_list)
+            bla = df[df['date'].isin(dates_list)]
+            print(bla.head())
+        # # selecting data by index
+        # row_data = df.loc[index, :]
+        # # counting rows in dataframe
+        # row_count = df.count()
+        # return row_data, row_count, index
 
     def get_price(self, index=0):
         # importing modules
@@ -32,7 +55,15 @@ class Simulation:
         conn = sqlite3.connect(self.file)
         # reading the database into  pandas dataframe
         df = pd.read_sql_query("SELECT * FROM {}".format(self.table_name), conn)
+        # filtering data
+        if self.date_range is not None:
+            condition_one = self.filter_value in df["date"]
+            list=[self.filter_value]
+            bla = df[df['date'].isin(list)]
+            print(bla.head())
+        # selecting data by index
         row_data = df.loc[index, :]
+        # counting rows in dataframe
         row_count=df.count()
         return row_data, row_count, index
 
@@ -41,6 +72,6 @@ class Simulation:
 
 
 if __name__ == "__main__":
-    sim = Simulation('IBM', interval='daily')
+    sim = Simulation('IBM', interval='daily', date_range=[[2004,1,1],[2004,12,31]])
     d,c,i=sim.get_price()
     print(d)
