13350bd5583ea6de6307d6d815ae61102dd335bc
Niklas <lord.voldemort1492@gmail.com>
2020-04-17 09:59:58 +0200

    Working simulation function of past done

--- SimulationStocks.py
+++ SimulationStocks.py
@@ -12,25 +12,34 @@ class Simulation:
         self.file = '/home/niklas/Desktop/TradingBot/StockData/StockData-{}.db'.format(symbol)
         # starting checking for right playing option
         if self.interval == 'daily':
-            GetStockData.get_data_daily(self.symbol, False)
+            a,b=GetStockData.get_data_daily(self.symbol, 'full', savingtoCsv=False)
             self.table_name = 'Daily' + self.symbol + 'daily'
-        if self.interval=='weekly':
-            GetStockData.get_data_weekly(self.symbol)
+        if self.interval == 'weekly':
+            GetStockData.get_data_weekly(self.symbol,savingtoCsv=False)
             self.table_name = 'Weekly' + self.symbol + 'weekly'
         if self.interval == 'monthly':
             GetStockData.get_data_monthly(self.symbol, False)
-            self.table_name='Monthly'+self.symbol+'monthly'
-        if self.interval == 'intrday':
+            self.table_name = 'Monthly' + self.symbol + 'monthly'
+        if self.interval == 'intraday':
             GetStockData.get_data_intraday(symbol, '1min', 'full', savingtoCsv=False)
             self.table_name = 'IntraDay' + self.symbol + 'min'
 
-    def get_price(self):
-
-        df = pd.read_sql_query("SELECT * FROM {}".format(tablename), conn)  # , index_col='date')
+    def get_price(self, index=0):
+        # importing modules
+        import sqlite3
+        import pandas as pd
+        # connecting to database
+        conn = sqlite3.connect(self.file)
+        # reading the database into  pandas dataframe
+        df = pd.read_sql_query("SELECT * FROM {}".format(self.table_name), conn)
+        row_data = df.loc[index, :]
+        row_count=df.count()
+        return row_data, row_count, index
 
     def __str__(self):
         return 'Simulation: Symbol: ' + self.symbol + ' Interval: ' + self.interval
 
 
 if __name__ == "__main__":
-    sim = Simulation('IBM')
+    sim = Simulation('IBM', interval='daily')
+    sim.get_price()
