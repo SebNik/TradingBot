ba6f2528059c43f7460b442ce72003254ea69499
Niklas <lord.voldemort1492@gmail.com>
2020-04-17 12:09:08 +0200

    Added vars

--- TradingModels.py
+++ TradingModels.py
@@ -4,11 +4,15 @@ from TradeStocks import Stock
 
 
 class Model(Stock):
+
     def __init__(self, symbol, simulation=True, start_acc=1000, fee=0.01, interval='daily'):
         super().__init__(symbol, start_acc=start_acc, fee=fee, check_if_exists=False)
         self.sim = simulation
-        self.interval=interval
+        self.interval = interval
 
+
+    def simple_high_low(self, open, close, high, low, volume):
+        print('Moin')
 
     def run(self):
         # this function will run all coded models
@@ -23,7 +27,11 @@ class Model(Stock):
             # for loop with real stuff is starting
             for i in range(0, int(count['date'])):
                 row, count, index = sim.get_price(index=i)
-                print(float(row['4. close']))
+                close = float(row['4. close'])
+                open = float(row['1. open'])
+                high = float(row['2. high'])
+                low = float(row['3. low'])
+                volume = float(row['5. volume'])
 
 
 if __name__ == '__main__':
