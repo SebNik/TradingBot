182b9b7bc397e98e6ab65602d9346f1223610ca4
Niklas <lord.voldemort1492@gmail.com>
2020-05-24 21:49:00 +0200

    working version 2.

--- GUI.py
+++ GUI.py
@@ -1,23 +1,27 @@
-# this file is responsible for all the graphics
-# importing modules used here
-import sys
-from qtpy import QtWidgets
-from ui.mainwindow import Ui_MainWindow
+# ------------------------------------------------------
+# ---------------------- main.py -----------------------
+# ------------------------------------------------------
+from PyQt5.QtWidgets import *
+from PyQt5.uic import loadUi
+
 from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
+
 import numpy as np
 import random
 
 
-class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+class MatplotlibWidget(QMainWindow):
 
-        self.setWindowTitle("Studierendenverwaltung")
+    def __init__(self):
+        QMainWindow.__init__(self)
 
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        loadUi("/home/niklas/Desktop/TradingBot/ui/mainwindow.ui", self)
 
-        self.ui.tab.pushButton.clicked.connect(self.update_graph())
+        self.setWindowTitle("PyQt5 & Matplotlib Example GUI")
+
+        self.pushButton.clicked.connect(self.update_graph)
+
+        self.addToolBar(NavigationToolbar(self.MplWidget.canvas, self))
 
     def update_graph(self):
         fs = 500
@@ -28,7 +32,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         cosinus_signal = np.cos(2 * np.pi * f * t)
         sinus_signal = np.sin(2 * np.pi * f * t)
-
+        print('lalalla')
         self.MplWidget.canvas.axes.clear()
         self.MplWidget.canvas.axes.plot(t, cosinus_signal)
         self.MplWidget.canvas.axes.plot(t, sinus_signal)
@@ -37,11 +41,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.MplWidget.canvas.draw()
 
 
-if __name__ == "__main__":
-   app = QtWidgets.QApplication(sys.argv)
-
-   window = MainWindow()
-
-   window.show()
-
-   sys.exit(app.exec_())
+app = QApplication([])
+window = MatplotlibWidget()
+window.show()
+app.exec_()
