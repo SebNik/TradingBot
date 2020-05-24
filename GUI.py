dc9b36784bc3e127f93015b48d3e08125aa2051a
Niklas <lord.voldemort1492@gmail.com>
2020-05-24 14:32:11 +0200

    basic setup of all ui mangment

--- GUI.py
+++ GUI.py
@@ -4,14 +4,20 @@ import sys
 from qtpy import QtWidgets
 from ui.mainwindow import Ui_MainWindow
 
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent = None):
+        super().__init__(parent)
+
+        self.setWindowTitle("Studierendenverwaltung")
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
 if __name__ == "__main__":
    app = QtWidgets.QApplication(sys.argv)
 
-   window = QtWidgets.QMainWindow()
-   window.setWindowTitle("Studierendenverwaltung")
-
-   ui_window = Ui_MainWindow()
-   ui_window.setupUi(window)
+   window = MainWindow()
 
    window.show()
 
